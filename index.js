6e8df87b9f7374ced66397b86bee2b6eb617abc6
nick <nickfchmail@gmail.com>
2026-01-30 22:04:36 +0800

    fix deployment issue

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import 'dotenv/config';
 
+import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 import mongoose from 'mongoose';
 import globalErrorHandler from './controllers/errorController.js';
 import authRouter from './routes/authRoute.js';
 import pokemonRouter from './routes/pokemonRoute.js';
 import reviewRouter from './routes/reviewRoute.js';
 import userRouter from './routes/userRoute.js';
-import AppError from './utils/appError.js';\
-import helmet from 'helmet';
-import compression from 'compression';
-import xss from 'xss';
-import mongoSanitize from 'express-mongo-sanitize'
+import AppError from './utils/appError.js';
 console.log('Loaded SUPABASE_URL:', process.env.SUPABASE_URL); // debug line
 
 const DB = process.env.MONGO_DB;
@@ -25,14 +25,12 @@ const app = express();
 
 //for security
 app.use(cors());
-app.options('*', cors());
-app.use(xss());
 app.use(compression());
 app.use(mongoSanitize());
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(helmet())
+app.use(helmet());
 app.use('/api/refresh', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/pokemons', pokemonRouter);
@@ -40,7 +38,7 @@ app.use('/api/review', reviewRouter);
 
 //error handling if no reaching point is found
 
-app.all('/*path', (req, res, next) => {
+app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
