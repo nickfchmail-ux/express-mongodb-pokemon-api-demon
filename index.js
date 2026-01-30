c95f7e60df9c029846be619ac6455949b8217cb3
nick <nickfchmail@gmail.com>
2026-01-30 21:52:13 +0800

    fix deployment issue and enhence security

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import authRouter from './routes/authRoute.js';
 import pokemonRouter from './routes/pokemonRoute.js';
 import reviewRouter from './routes/reviewRoute.js';
 import userRouter from './routes/userRoute.js';
-import AppError from './utils/appError.js';
+import AppError from './utils/appError.js';\
+import helmet from 'helmet';
+import compression from 'compression';
+import xss from 'xss';
+import mongoSanitize from 'express-mongo-sanitize'
 console.log('Loaded SUPABASE_URL:', process.env.SUPABASE_URL); // debug line
 
 const DB = process.env.MONGO_DB;
@@ -18,8 +22,17 @@ mongoose.connect(DB).then((con) => {
 });
 
 const app = express();
+
+//for security
+app.use(cors());
+app.options('*', cors());
+app.use(xss());
+app.use(compression());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet())
 app.use('/api/refresh', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/pokemons', pokemonRouter);
